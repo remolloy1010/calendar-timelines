0e9cec4b6e33c70f906665982799fd8080683b89
remolloy1010 <remolloy101@gmail.com>
2020-08-24 09:05:35 -0600

    added switch buttons on menu pane

--- src/Settings.js
+++ src/Settings.js
@@ -2,6 +2,10 @@ import React from 'react';
 import SettingsIcon from '@material-ui/icons/Settings';
 import { Menu, MenuItem, IconButton } from '@material-ui/core';
 import { useAnchorElStateApi } from "use-state-api-hooks";
+import Switch from '@material-ui/core/Switch';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
 
 
 
@@ -19,6 +23,12 @@ const styles = {
 
 export default function Settings() {
     const { anchorEl, setAnchorEl, clearAnchorEl } = useAnchorElStateApi(null);
+    const [checked, setChecked] = React.useState(false);
+
+    const toggleChecked = () => {
+      setChecked((prev) => !prev);
+    };
+
     return( 
         <div style={styles.root}> 
         <IconButton onClick={setAnchorEl}> <SettingsIcon color='white' fill='white' styles={styles.settingsIcon} /></IconButton>
@@ -28,9 +38,51 @@ export default function Settings() {
         open={Boolean(anchorEl)}
         onClose={clearAnchorEl}
       >
-        <MenuItem onClick={clearAnchorEl}>Profile</MenuItem>
-        <MenuItem onClick={clearAnchorEl}>My account</MenuItem>
-        <MenuItem onClick={clearAnchorEl}>Logout</MenuItem>
+        <MenuItem onClick={clearAnchorEl}><FormControl component="fieldset">
+          <FormGroup aria-label="position" row>
+            <FormControlLabel
+              value="# of Projects"
+              control={<Switch color="primary" checked={checked} onChange={toggleChecked} />} 
+              label="# of Projects"
+              labelPlacement="end"
+            />
+            </FormGroup>
+          </FormControl>
+        </MenuItem>
+        <MenuItem onClick={clearAnchorEl}><FormControl component="fieldset">
+          <FormGroup aria-label="position" row>
+            <FormControlLabel
+              value="rev"
+              control={<Switch color="primary" />}
+              label="Avg Monthly Revenue Impact"
+              labelPlacement="end"
+            />
+            </FormGroup>
+          </FormControl>
+        </MenuItem>
+        <MenuItem onClick={clearAnchorEl}><FormControl component="fieldset">
+          <FormGroup aria-label="position" row>
+            <FormControlLabel
+              value="success"
+              control={<Switch color="primary" />}
+              label="Success Rate"
+              labelPlacement="end"
+            />
+            </FormGroup>
+          </FormControl>
+        </MenuItem>
+        <MenuItem onClick={clearAnchorEl}><FormControl component="fieldset">
+          <FormGroup aria-label="position" row>
+            <FormControlLabel
+              value="slip"
+              control={<Switch color="primary" />}
+              label="Avg Slip Rate"
+              labelPlacement="end"
+            />
+            </FormGroup>
+          </FormControl>
+        </MenuItem>
+
       </Menu>
         </div>
     )
